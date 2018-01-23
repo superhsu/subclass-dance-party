10a0e28caa60f18b4dec7c5c516c2fb79baedf58
superhsu <31141265+superhsu@users.noreply.github.com>
2018-01-22 20:07:15 -0800

    2nd commit

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,13 +1,14 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+  console.log(this);
   makeDancer.call(this, top, left, timeBetweenSteps);
   this.$node = $('<span class="blinkyDancer"></span>');
-  //this.setPosition(top, left);
+  this.setPosition(this.top, this.left);
   this.step(); 
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer; 
 makeBlinkyDancer.prototype.step = function() {
-    this.$node.toggle();
+    $node.toggle();
     makeDancer.prototype.step.call(this);
   };
--- src/dancer.js
+++ src/dancer.js
@@ -1,20 +1,23 @@
 
 var makeDancer = function(top, left, timeBetweenSteps) {
-  console.log(this);
+  this.top = top; 
+  this.left = left; 
   this.$node = $('<span class="dancer"></span>');
   this.timeBetweenSteps = timeBetweenSteps;
-  this.setPosition(top, left);
+  this.setPosition(this.top, this.left);
 };
 
   makeDancer.prototype.step = function(timeBetweenSteps) {
-   setTimeout(this.step.bind(this), this.timeBetweenSteps);
+  var step = this.step.bind(this);
+  setTimeout(step, this.timeBetweenSteps);
+  this.$node.toggle();
   };
   
 
   makeDancer.prototype.setPosition = function(top, left) {
     var styleSettings = {
-      top: top,
-      left: left
+      top: this.top,
+      left: this.left
     };
     this.$node.css(styleSettings);
   };
